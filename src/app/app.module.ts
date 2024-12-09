00b4031d85a165942202872101b8a73285f24256
LemanSachs29 <163987047+LemanSachs29@users.noreply.github.com>
2024-12-09 08:39:38 +0100

    commit de domingo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { FichaProductoComponent } from './ficha-producto/ficha-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ListaProductosComponent,
+    FichaProductoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ficha-producto/ficha-producto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ficha-producto',
+  templateUrl: './ficha-producto.component.html',
+  styleUrls: ['./ficha-producto.component.scss']
+})
+export class FichaProductoComponent {
+
+}
