abac2fe86ab369f98adc7d7e9ca5a8a6b13ed244
LemanSachs29 <163987047+LemanSachs29@users.noreply.github.com>
2024-12-10 13:34:22 +0100

    añadidio routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { FichaProductoComponent } from './ficha-producto/ficha-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Main', pathMatch: 'full' },
+  { path: 'Main', component: MainComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'lista-productos', component: ListaProductosComponent},
+  { path: 'ficha-producto', component: FichaProductoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
