764c5582c0927b7a4e8c742ec165804fb3eb31f1
Jaime Guerrero <163987047+LemanSachs29@users.noreply.github.com>
2024-12-13 13:20:07 +0100

    fin del clon

--- src/app/lista-productos/lista-productos.component.ts
+++ src/app/lista-productos/lista-productos.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-/**Proporciona acceso a información sobre una ruta asociada a un componente cargado. Es útil para acceder a parámetros de la URL, datos de la ruta, etc */
-import { ActivatedRoute } from '@angular/router';
+/**Proporciona acceso a información sobre una ruta asociada a un componente cargado. Es útil para acceder a parámetros de la URL, datos de la ruta, etc
+ * El servicio Router se usa para navegar programáticamente entre rutas. Es útil cuando necesitas redirigir al usuario a otra págína desde el código, en lugar de usar enlaces HTML(routerLink)
+ */
+
+import { ActivatedRoute, Router } from '@angular/router';
 
 /** Importas el servicio que has creado para manejar operaciones relacionadas con los productos. */
 import { ProductosServiceService } from 'src/app/productos-service.service';
@@ -18,11 +21,12 @@ export class ListaProductosComponent implements OnInit{
 
   constructor(
     private route: ActivatedRoute, // Para obtener el parámetro de la URL
-    private productosService: ProductosServiceService // Servicio para obtener los productos
+    private productosService: ProductosServiceService, // Servicio para obtener los productos
+    private router: Router //para navegar a otras rutas
   ) {}
 
 
-  /**Metodo se encargad de obtener todos los producots y luego filtrar aquellos que pertenecen a a la categoría especificada por parámetro. No devuelve nada */
+  /**Metodo se encargado de obtener todos los producots y luego filtrar aquellos que pertenecen a a la categoría especificada por parámetro. No devuelve nada */
   cargarProductosPorCategoria(id_categoria: number): void {
     this.productosService.getProductos()
       .then((data) => {
@@ -40,6 +44,10 @@ export class ListaProductosComponent implements OnInit{
       this.id_categoria = +params['id_categoria']; // Convertir el parámetro a número
       this.cargarProductosPorCategoria(this.id_categoria);
     });
+  }
+
+  verFichaProducto(id_producto: number): void {
+    this.router.navigate(['/ficha-producto', id_producto]);
   }
 
 
